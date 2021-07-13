de3a4478271df3b5c125c4fad54531e554b4cddd
hanhbui27052000 <hanhbui27052000@gmail.com>
2021-07-13 13:37:01 +0700

    add auth to project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    @include('layouts.head')
+</head>
+
+<body>
+    <div id="app">
+        @include('layouts.menu')
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
